b542ca16591fe8a5e7680362b2e22504a80dd310
Mike Araujo <miguelant.araujo@gmail.com>
2019-06-08 10:20:51 -0400

    Small refactoring. Preparing to make this public

--- app.js
+++ app.js
@@ -1,51 +1,56 @@
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
 // const session = require('express-session')
-const passport = require('passport')
-const flash = require('connect-flash')
-const validator = require('express-validator')
-require('dotenv').config()
+const passport = require("passport");
+const flash = require("connect-flash");
+const validator = require("express-validator");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-mongoose.connect('mongodb://localhost:27017/todo-vue', {useNewUrlParser: true})
-    .then(db => console.log(`Successfully connected to ${db.connection.db.databaseName}`))
-    .catch(err => console.error(err))
+mongoose
+  .connect("mongodb://localhost:27017/todo-vue", { useNewUrlParser: true })
+  .then(db =>
+    console.log(
+      `Successfully connected to database '${db.connection.db.databaseName}'`
+    )
+  )
+  .catch(err => console.error(err));
 
-    // Settings
-app.set('port', process.env.PORT || 3000)
+// Settings
+app.set("port", process.env.PORT || 3000);
 
 // Middlewares
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(validator())
-app.use(cors())
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(validator());
+app.use(cors());
 // app.use(session({
 //     secret: 'dis is da bomb',
 //     resave: false,
 //     saveUninitialized: false
 // }))
-require('./config/passport')(passport)
-app.use(passport.initialize())
+require("./config/passport")(passport);
+app.use(passport.initialize());
 // app.use(passport.session())
-app.use(flash())
+app.use(flash());
 
 app.use((req, res, next) => {
-    res.locals.login = req.isAuthenticated()
-    res.locals.user = req.user
-    next()
-})
+  res.locals.login = req.isAuthenticated();
+  res.locals.user = req.user;
+  next();
+});
 
 // Routes
-app.use('/api/todos', require('./routes/todos'))
-app.use('/api/users', require('./routes/users'))
+app.use("/api/todos", require("./routes/todos"));
+app.use("/api/users", require("./routes/users"));
 
 // Static Files
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
 // Start server
-app.listen(app.get('port'), () => {
-    console.log('Server started at http://localhost:' + app.get('port'))
-})
+app.listen(app.get("port"), () => {
+  console.log("Server started at http://localhost:" + app.get("port"));
+});
